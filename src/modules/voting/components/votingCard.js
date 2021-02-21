dfb7719f997a7cb40674a680493c5b0f389f2c1e
ohager <oliver.hager@dextra-sw.com>
2021-02-21 19:00:51 -0300

    wip: card content - charts

--- src/modules/voting/components/votingCard.js
+++ src/modules/voting/components/votingCard.js
@@ -8,22 +8,48 @@
      * @param {$rootScope.Scope} $scope
      * @return {VotingCard}
      */
-    const controller = function (Base, $scope) {
+    const controller = function (Base, $scope, waves) {
 
         class VotingCard extends Base {
 
             pollData = {}
 
+            relativeElapsedTime = 0.0
+
             constructor() {
                 super($scope);
             }
+
+            async $onInit() {
+                const height = await waves.node.height();
+                this.relativeElapsedTime = Math.min(height / this.pollData.end, 1.0);
+            }
+
+            getCurrentVotesAsNormalized() {
+                // TODO: array of votes normalized to 1.0
+                return [
+                    {
+                        label: 'option1',
+                        value: 0.34
+                    },
+                    {
+                        label: 'option2',
+                        value: 0.2
+                    },
+                    {
+                        label: 'option3',
+                        value: 0.46
+                    }
+                ];
+            }
+
 
         }
 
         return new VotingCard();
     };
 
-    controller.$inject = ['Base', '$scope'];
+    controller.$inject = ['Base', '$scope', 'waves'];
 
     angular.module('app.voting').component('wVotingCard', {
         bindings: {
